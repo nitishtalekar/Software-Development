8857112d82a7d005f403cfe01229755936f6a591
sagarambilpure <>
2019-09-28 11:56:16 +0530

    deleted waste

--- RGIT_v_old/includes/dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$database_host = 'localhost';
-$database_username = 'root';
-$database_password = '';
-$database_name = 'rgitdb';
-
-$conn = new mysqli($database_host, $database_username, $database_password, $database_name) ;
-?>
